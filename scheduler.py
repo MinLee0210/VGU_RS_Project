d663eeea4892cc869514e69e7c000252b219fc29
MinLee0210 <bastreet124@gmail.com>
2023-05-16 14:30:33 +0700

    Update Cooperative Scheduling

--- scheduler.py
+++ scheduler.py
@@ -15,12 +15,11 @@ class Scheduler:
     SCH_MAX_TASKS = 40
     SCH_tasks_G = []
     current_index_task = 0
-
     def __int__(self):
-        return
+        self.current_task = 0
 
     def SCH_Init(self):
-        self.current_index_task = 0
+        self.current_task = 0
 
     def SCH_Add_Task(self, pFunction, DELAY, PERIOD):
         if self.current_index_task < self.SCH_MAX_TASKS:
@@ -32,18 +31,30 @@ class Scheduler:
             print("PrivateTasks are full!!!")
 
     def SCH_Update(self):
-        for i in range(0, len(self.SCH_tasks_G)):
-            if self.SCH_tasks_G[i].Delay > 0:
-                self.SCH_tasks_G[i].Delay -= 1
-            else:
-                self.SCH_tasks_G[i].Delay = self.SCH_tasks_G[i].Period
-                self.SCH_tasks_G[i].RunMe += 1
+        # for i in range(0, len(self.SCH_tasks_G)):
+        #     if self.SCH_tasks_G[i].Delay > 0:
+        #         self.SCH_tasks_G[i].Delay -= 1
+        #     else:
+        #         self.SCH_tasks_G[i].Delay = self.SCH_tasks_G[i].Period
+        #         self.SCH_tasks_G[i].RunMe += 1
+        # self.current_task = 0
+        if self.SCH_tasks_G[self.current_task].Delay > 0:
+            self.SCH_tasks_G[self.current_task].Delay -= 1
+        else:
+            self.SCH_tasks_G[self.current_task].Delay = self.SCH_tasks_G[self.current_task].Period
+            self.SCH_tasks_G[self.current_task].RunMe += 1
+        # self.current_task = (self.current_task + 1) % len(self.SCH_tasks_G)
 
     def SCH_Dispatch_Tasks(self):
-        for i in range(0, len(self.SCH_tasks_G)):
-            if self.SCH_tasks_G[i].RunMe > 0:
-                self.SCH_tasks_G[i].RunMe -= 1
-                self.SCH_tasks_G[i].pTask()
+        # for i in range(0, len(self.SCH_tasks_G)):
+        #     if self.SCH_tasks_G[i].RunMe > 0:
+        #         self.SCH_tasks_G[i].RunMe -= 1
+        #         self.SCH_tasks_G[i].pTask()
+        # self.current_task = 0
+        if self.SCH_tasks_G[self.current_task].RunMe > 0:
+            self.SCH_tasks_G[self.current_task].RunMe -= 1
+            self.SCH_tasks_G[self.current_task].pTask()
+        self.current_task = (self.current_task + 1) % len(self.SCH_tasks_G)
 
     def SCH_Delete(self, aTask):
         return
